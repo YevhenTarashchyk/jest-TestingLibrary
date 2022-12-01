14514352cfc2ed1a7a10be59be117f89973036ee
Tarashchyk <maze.ultramarine@gmail.com>
2022-12-01 11:52:04 +0200

    checkbox initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,24 +2,32 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('button has correct initial color, and updates when clicked', () => {
-  render(<App />);
+test( 'button has correct initial color, and updates when clicked', () => {
+  render( <App /> );
 
   // find an element with a role of button and text of 'Change to blue'  
-  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  const colorButton = screen.getByRole( 'button', { name: 'Change to blue' } );
 
   // expect the background color to be red
-  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+  expect( colorButton ).toHaveStyle( { backgroundColor: 'red' } )
 
   // click button
-  fireEvent.click(colorButton);
+  fireEvent.click( colorButton );
 
   // expect the background color to be blue 
-  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+  expect( colorButton ).toHaveStyle( { backgroundColor: 'blue' } );
 
   // expect the button text to be 'Change to red'
-  expect(colorButton).toHaveTextContent('Change to red');
-});
+  expect( colorButton ).toHaveTextContent( 'Change to red' );
+} );
+
+test( 'initial condition', () => {
+  render( <App /> );
+  // check that the button starts out enabled
+  // const colorButton = screen.getByRole( 'button', { name: 'Change to blue' } );
+
+  // check that the checkbox stars out unchecked
+} )
 
 
 
